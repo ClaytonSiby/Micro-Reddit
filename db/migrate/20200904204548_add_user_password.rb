0ff35ddde2c7ebce87231836da12aed16839fac2
Clayton Siby <claytonsiby@gmail.com>
2020-09-04 22:51:33 +0200

    add ( password & email_address columns to users table

---
+++ db/migrate/20200904204548_add_user_password.rb
@@ -0,0 +1,5 @@
+class AddUserPassword < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :password, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_04_201536) do
+ActiveRecord::Schema.define(version: 2020_09_04_204548) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email_address"
+    t.string "password"
   end
 
 end
