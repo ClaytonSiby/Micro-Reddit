52b1b263891bb9583e1dde70225149aed78e6eca
Clayton Siby <claytonsiby@gmail.com>
2020-09-04 23:54:40 +0200

    add validation for email_address

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     validates :name, presence: true
-    validates :password, presence: true
-    validates :email_address, presence: true, confirmation: { case_sensitive: false }
+    validates :password, presence: true, length: { in: 6..12 }
+    validates :email_address, presence: true, uniqueness: true, confirmation: { case_sensitive: false },format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ }
 end
